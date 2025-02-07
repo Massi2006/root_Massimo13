0c9e03ce24cc31e2786d8bd588e4fb1b2da01e23
Massi2006 <2474613@edu.vaniercollege.qc.ca>
2025-02-07 08:35:00 -0500

    mesage

---
+++ public_html/web2/js/2d_transform.js
@@ -0,0 +1,11 @@
+// 2d_transform.js
+
+console.log('2d_transform.js loaded');
+
+//create a selector
+let scudetto = document.querySelector("#car");
+
+scudetto.addEventListener('click', function (event) {
+  //console.log(this.id);
+  this.classList.toggle('move-right');
+})
